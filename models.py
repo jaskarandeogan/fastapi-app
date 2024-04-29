0bdcc785efd252204aca7a7597e6202c165e021b
jaskarandeogan <jaskaran2k15@gmail.com>
2024-04-28 23:10:04 -0700

    remove duplicate code

--- models.py
+++ models.py
@@ -1,16 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String, Boolean
+from database import Base
 
 class Todo(Base):
     __tablename__ = "todos"
@@ -19,6 +8,3 @@ class Todo(Base):
     title = Column(String, index=True)
     description = Column(String, index=True)
     completed = Column(Boolean, default=False)
-
-def create_db_and_tables():
-    Base.metadata.create_all(bind=engine)
